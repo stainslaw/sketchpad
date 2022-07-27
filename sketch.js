7bbca80bf64ec3ab44f42484e9389890ff145753
Omar-Hakim Echehamta <hakimasila@gmail.com>
2022-07-27 12:43:52 +0100

    Add function makeRows

--- sketch.js
+++ sketch.js
@@ -1,10 +1,25 @@
 const containerDiv = document.querySelector("#container");
-let gridCount;
 
-while (gridCount < 32) {
-    const gridDivs = document.createElement("div");
-    gridDivs.classList = "HELLo!";
-    containerDiv.appendChild(gridDivs);
-    gridCount++;
-    console.log(gridCount);
+function makeRows (rows, columns) {
+    containerDiv.style.setProperty("--grid-rows", rows);
+    containerDiv.style.setProperty("--grid-columns", columns);
+    console.log(containerDiv);
+    console.log(rows);
+    console.log(columns);
+    for (i = 0; i < (rows * columns); i++) {
+        let square = document.createElement("div");
+        square.innterTExt = (i + 1);
+        containerDivDiv.appendChild(square).className = "grid-item";
+    }
 }
+
+makeRows(16, 16);
+
+// while (gridCount < 32) {
+ //   const gridDivs = document.createElement("div");
+ //   gridDivs.classList.add("content");
+ //   gridDivs.textContent = "a";
+ //   containerDiv.appendChild(gridDivs);
+ //   gridCount++;
+ //   console.log(gridCount);
+// }
