70ae6896a2dd7263754ca7ad938240b34317f91a
Omar-Hakim Echehamta <hakimasila@gmail.com>
2022-07-27 13:04:26 +0100

    Correct mistake

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 const containerDiv = document.querySelector("#container");
 
-function makeRows (rows, columns) {
+function makeRows(rows, columns) {
     containerDiv.style.setProperty("--grid-rows", rows);
     containerDiv.style.setProperty("--grid-columns", columns);
     console.log(containerDiv);
@@ -8,18 +8,10 @@ function makeRows (rows, columns) {
     console.log(columns);
     for (i = 0; i < (rows * columns); i++) {
         let square = document.createElement("div");
-        square.innterTExt = (i + 1);
-        containerDivDiv.appendChild(square).className = "grid-item";
+        square.innerText = (i + 1);
+        containerDiv.appendChild(square).className = "grid-item";
     }
 }
 
 makeRows(16, 16);
 
-// while (gridCount < 32) {
- //   const gridDivs = document.createElement("div");
- //   gridDivs.classList.add("content");
- //   gridDivs.textContent = "a";
- //   containerDiv.appendChild(gridDivs);
- //   gridCount++;
- //   console.log(gridCount);
-// }
