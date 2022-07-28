de57e240877fe065fcd3f08548486da34a295fbd
Omar-Hakim Echehamta <hakimasila@gmail.com>
2022-07-28 15:04:52 +0100

    Add event listener mouse over to create a trail of black boxes

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ const containerDiv = document.querySelector("#container");
 function makeRows(rows, columns) {
     containerDiv.style.setProperty("--grid-rows", rows);
     containerDiv.style.setProperty("--grid-columns", columns);
-    console.log(containerDiv);
-    console.log(rows);
-    console.log(columns);
     for (i = 0; i < (rows * columns); i++) {
         let square = document.createElement("div");
-        square.innerText = (i + 1);
         containerDiv.appendChild(square).className = "grid-item";
+        square.addEventListener("mouseover", () => { 
+            square.style.backgroundColor = "black";
+            //console.log(square);
+        })
     }
 }
 
